4a9ec0d067786858db39e0c588e9270654ad5171
Mithunchandran <135313170+Mithunchandran@users.noreply.github.com>
2024-05-10 15:20:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,121 @@
+$('#owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: true,
+    nav: true,
+    items: 4,
+})
+
+
+function setProgress(percent) {
+    const progress = document.querySelector('.progress');
+    progress.style.transform = `rotate(${(percent / 100) * 360}deg)`;
+}
+
+// Example usage: setProgress(50); // Will set progress to 50%
+
+
+// -----------------------------tab---------------------------------//
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
+
+
+
+// TYPED TEXT   
+  var typed = new Typed('#element', {
+    strings: ['Front-End Developer', 'Web Designer','WordPress Developer'],
+    typeSpeed: 100,
+    backSpeed:50,
+    loop:true,
+    smartBackspace: true,
+  });
+  console.log(typed);
+
+
+
+
+  // init
+var maxx = document.body.clientWidth;
+var maxy = document.body.clientHeight;
+var halfx = maxx / 2;
+var halfy = maxy / 2;
+var canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+canvas.width = maxx;
+canvas.height = maxy;
+var context = canvas.getContext("2d");
+var dotCount = 200;
+var dots = [];
+// create dots
+for (var i = 0; i < dotCount; i++) {
+  dots.push(new dot());
+}
+
+// dots animation
+function render() {
+  context.fillStyle = "#000000";
+  context.fillRect(0, 0, maxx, maxy);
+  for (var i = 0; i < dotCount; i++) {
+    dots[i].draw();
+    dots[i].move();
+  }
+  requestAnimationFrame(render);
+}
+
+// dots class
+// @constructor
+function dot() {
+  
+  this.rad_x = 2 * Math.random() * halfx + 1;
+  this.rad_y = 1.2 * Math.random() * halfy + 1;
+  this.alpha = Math.random() * 360 + 1;
+  this.speed = Math.random() * 100 < 50 ? 1 : -1;
+  this.speed *= 0.1;
+  this.size = Math.random() * 5 + 1;
+  this.color = Math.floor(Math.random() * 256);
+  
+}
+
+// drawing dot
+dot.prototype.draw = function() {
+  
+  // calc polar coord to decart
+  var dx = halfx + this.rad_x * Math.cos(this.alpha / 180 * Math.PI);
+  var dy = halfy + this.rad_y * Math.sin(this.alpha / 180 * Math.PI);
+  // set color
+  context.fillStyle = "rgb(" + this.color + "," + this.color + "," + this.color + ")";
+  // draw dot
+  context.fillRect(dx, dy, this.size, this.size);
+  
+};
+
+// calc new position in polar coord
+dot.prototype.move = function() {
+  
+  this.alpha += this.speed;
+  // change color
+  if (Math.random() * 100 < 50) {
+    this.color += 1;
+  } else {
+    this.color -= 1;
+  }
+  
+};
+
+// start animation
+render();
+
+ 
